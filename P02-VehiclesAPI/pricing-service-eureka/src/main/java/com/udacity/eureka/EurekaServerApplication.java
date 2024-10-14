6d6f51966c419d68a91a5773bdbee986a645aac7
Andrew Chan <chan.andrew1995@gmail.com>
2024-10-13 23:13:33 -0400

    Turned pricing service into a microservice

--- P02-VehiclesAPI/pricing-service-eureka/src/main/java/com/udacity/eureka/EurekaServerApplication.java
+++ P02-VehiclesAPI/pricing-service-eureka/src/main/java/com/udacity/eureka/EurekaServerApplication.java
@@ -1,19 +1,19 @@
-package com.udacity.pricing;
+package com.udacity.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PricingServiceApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PricingServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
